1ebced1b960c002b4a6916d80b630205990a2a50
Ben Briggs <therealbenbriggs@hotmail.com>
2014-01-01 01:16:00 +0000

    event stream -> map stream

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
-var es = require('event-stream');
+var map = require('map-stream');
 
 module.exports = function() {
     'use strict';
-    return es.map(function(file, cb) {
+    return map(function(file, cb) {
         console.log(String(file.contents));
         cb(null, file);
     });
