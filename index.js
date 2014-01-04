1b21759abc241297208f30473bcb7f1bf5bda6af
Ben Briggs <therealbenbriggs@hotmail.com>
2014-01-04 16:29:39 +0000

    Code style & lint errors fixed.

--- index.js
+++ index.js
@@ -1,18 +1,21 @@
+/* jshint node:true */
+
+'use strict';
+
 var PassThrough = require('stream').PassThrough;
 
 module.exports = function() {
-    'use strict';
-    var stream = new PassThrough({objectMode: true});
+    var stream = new PassThrough({ objectMode: true });
     stream.on('data', function(file) {
-      if(file.isNull()) return;
-    
-      if(file.isBuffer()) {
-        process.stdout.write(file.contents);
-      } else {
-        file.contents.pipe(process.stdout);
-        file.contents = file.contents.pipe(new PassThrough());
-      }
-
+        if (file.isNull()) {
+            return;
+        }
+        if (file.isBuffer()) {
+            process.stdout.write(file.contents);
+        } else {
+            file.contents.pipe(process.stdout);
+            file.contents = file.contents.pipe(new PassThrough());
+        }
     });
     return stream;
 };
--- tests/index.mocha.js
+++ tests/index.mocha.js
@@ -1,84 +1,81 @@
+/* jshint node: true */
+/* global describe, it, before, beforeEach, after, afterEach */
+
 'use strict';
 
-var assert = require('assert')
-  , es = require('event-stream')
-  , gutil = require('gulp-util')
-  , PassThrough = require('stream').PassThrough
-  , cat = require(__dirname + '/../index.js')
-;
+var assert = require('assert'),
+    es = require('event-stream'),
+    gutil = require('gulp-util'),
+    PassThrough = require('stream').PassThrough,
+    cat = require(__dirname + '/../index.js');
 
-describe('gulp-cat should work', function() {
 
-  it('in buffer mode', function(done) {
+describe('gulp-cat', function() {
+    it('should work in buffer mode', function(done) {
+        var stream = cat();
+        var fakeBuffer = new Buffer("wadup");
+        var fakeFile = new gutil.File({
+            contents: fakeBuffer
+        });
 
-      var stream = cat();
-      var fakeBuffer = new Buffer("wadup");
-      var fakeFile = new gutil.File({
-        contents: fakeBuffer
-      });
+        var fakeBuffer2 = new Buffer("doe");
+        var fakeFile2 = new gutil.File({
+            contents: fakeBuffer2
+        });
 
-      var fakeBuffer2 = new Buffer("doe");
-      var fakeFile2 = new gutil.File({
-        contents: fakeBuffer2
-      });
+        stream.on('data', function(newFile) {
+            if (newFile === fakeFile) {
+                assert.equal(fakeBuffer, newFile.contents);
+            } else {
+                assert.equal(fakeBuffer2, newFile.contents);
+            }
+        });
 
-      stream.on('data', function(newFile){
-        if(newFile === fakeFile) {
-          assert.equal(fakeBuffer, newFile.contents);
-        } else {
-          assert.equal(fakeBuffer2, newFile.contents);
-        }
-      });
+        stream.on('end', function() {
+            done();
+        });
 
-      stream.on('end', function() {
-        done();
-      });
+        stream.write(fakeFile);
+        stream.write(fakeFile2);
+        stream.end();
+    });
 
-      stream.write(fakeFile);
-      stream.write(fakeFile2);
-      stream.end();
+    it('should work in stream mode', function(done) {
+        var stream = cat();
+        var fakeStream = new PassThrough();
+        var fakeFile = new gutil.File({
+            contents: fakeStream
+        });
+        fakeStream.write(new Buffer("wa"));
+        fakeStream.write(new Buffer("dup"));
+        fakeStream.end();
 
-  });
+        var fakeStream2 = new PassThrough();
+        var fakeFile2 = new gutil.File({
+            contents: fakeStream2
+        });
+        fakeStream2.write(new Buffer("do"));
+        fakeStream2.write(new Buffer("e"));
+        fakeStream2.end();
 
-  it('in stream mode', function(done) {
+        stream.on('data', function(newFile){
+            if (newFile === fakeFile) {
+                newFile.pipe(es.wait(function(err, data) {
+                    assert.equal("wadup", data);
+                }));
+            } else {
+                newFile.pipe(es.wait(function(err, data) {
+                    assert.equal("doe", data);
+                }));
+            }
+        });
 
-      var stream = cat();
-      var fakeStream = new PassThrough();
-      var fakeFile = new gutil.File({
-        contents: fakeStream
-      });
-      fakeStream.write(Buffer("wa"));
-      fakeStream.write(Buffer("dup"));
-      fakeStream.end();
+        stream.on('end', function() {
+          done();
+        });
 
-      var fakeStream2 = new PassThrough();
-      var fakeFile2 = new gutil.File({
-        contents: fakeStream2
-      });
-      fakeStream2.write(Buffer("do"));
-      fakeStream2.write(Buffer("e"));
-      fakeStream2.end();
-
-      stream.on('data', function(newFile){
-        if(newFile === fakeFile) {
-          newFile.pipe(es.wait(function(err, data){
-            assert.equal("wadup", data);
-          }));
-        } else {
-          newFile.pipe(es.wait(function(err, data){
-            assert.equal("doe", data);
-          }));
-        }
-      });
-
-      stream.on('end', function() {
-        done();
-      });
-
-      stream.write(fakeFile);
-      stream.write(fakeFile2);
-      stream.end();
-
-  });
-
+        stream.write(fakeFile);
+        stream.write(fakeFile2);
+        stream.end();
+    });
 });
